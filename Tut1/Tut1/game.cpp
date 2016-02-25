16390dce5c3e8e7e6aea4ad21fb86afea27b63ab
Sanath Maharaj <sanathm7@gmail.com>
2016-02-25 20:49:08 +0200

    fix number error

--- Tut1/Tut1/game.cpp
+++ Tut1/Tut1/game.cpp
@@ -4,16 +4,21 @@ using namespace std;
 
 int main()
 {
+	bool mult;
 	for (int i = 1; i <= 20; i++)
 	{
+		mult = false;
 		if (i % 3 == 0){
 			cout << "Fizz ";
+			mult = true;
 		}
 		if (i % 5 == 0){
 			cout << "Buzz ";
+			mult = true;
 		}
-		else{
-			cout << i << endl;
+		if (mult == false){
+			cout << i;
 		}
+		cout << endl;
 	}
 }
